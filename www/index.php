834675a92fa17d8bdb92c1a87dc8f8456ce54475
Alexandre <musachi.jakatera@gmail.com>
2018-11-28 13:43:15 +0100

    [Front-end] Ajout du squelette HTML + Esthétique CSS (Révision possible)
    - Ajout des images de fond + Animation + CSS
    - Squelette HTML complet

--- www/index.php
+++ www/index.php
@@ -1,21 +1,37 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Netflex</title>
-    <meta name="viewport" content="width=device-width">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NETPLEX</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:700" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <h1>Netflex</h1>
-    <input type="text" id="valeur" placeholder="Ajoutez une série" title="Nom de la série" />
-    <p id="text"></p>
-    <p id="bouton">click</p>
-    <div class="outer">
-              <div class="inner">
-              <header id="TMDBtitle"></header>
-              </div>
-            </div>
+  </head>
+  <body>
+    <div class="slider">
+      <div class="load">
+      </div>
+
+      <div class="content">
+
+        <div class="principal">
+          <h1>NETFLEX</h1>
+        </div>
+
+          <form>
+             <input type="text" name="pseudo" id="pseudo" placeholder="Rechercher une série" onfocus="this.placeholder = ''" onblur= "this.placeholder = 'Rechercher une série'" size="30" maxlength="70" autocomplete="off" />
+             <input type="submit" value="AJOUTER" />
+          </form>
+
+          <div class="card">
+            <img src="IMG/test.jpg" alt="Arrow" >
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
 
       <ul id="output">
 
