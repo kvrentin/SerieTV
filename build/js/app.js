f0b4f9263fa65eed9ce67c99830f96433b90dcbb
Corentin <corentin.moiszan1@gmail.com>
2018-11-28 13:36:31 +0100

    Ajout des fonctionnalités Release 1 :
    - Connexion à l'API The Movie DataBase
    - Récupération de l'image
    - Autocompletion avec jQuery-UI

--- build/js/app.js
+++ build/js/app.js
@@ -1,23 +1,61 @@
+//@prepros-prepend ../../node_modules/jquery/dist/jquery.js
+//@prepros-prepend ../../node_modules/jquery-ui-dist/jquery-ui.js
+
 $(document).ready(function() {
-  function cycleBackgrounds() {
-      var index = 0;
+var key = 'api_key=13f6a6b30635eaee1ec0a788dc675c17';
+var ImgPath = "//image.tmdb.org/t/p/w300";
 
-      $imageEls = $('.container .slide'); // Get the images to be cycled.
+$(function(){
+  $( "#valeur" ).autocomplete({
+    source: function(request, response) {
+      $.getJSON("http://api.themoviedb.org/3/search/tv?" + key , { query: request.term }, function(tvshow){
+            response(tvshow.results.slice(0, 4));
+       });
+    },
+  minLength:	2,
+  delay:		500,
+  focus: 		function( event, tvshow ) {
+          $(this).val(tvshow.item.name);
+          return false;
+        },
+  select: 	function( event, tvshow) {
+    var thisTv = tvshow.item;
+    html= "<div class='outer'><div class='inner'><span class='left'> <img  width='45' height='68' src=" + ImgPath  + thisTv.poster_path + "><\/span><span class='right'><strong>&nbsp; &nbsp; " + thisTv.name;
 
-      setInterval(function () {
-          // Get the next index.  If at end, restart to the beginning.
-          index = index + 1 < $imageEls.length ? index + 1 : 0;
+    $('#response').html(html);
+    var link = "http://api.themoviedb.org/3/tv/" +  thisTv.id;
+    var tmdb_id;
+        $.ajax({
+        type: 'GET',
+        url: link + tmdb_id + "?"+key,
+        async: false,
+        jsonpCallback: 'testing',
+        contentType: 'application/json',
+        dataType: 'jsonp',
+        success: function(json) {
+          console.dir(json);
+            document.getElementById('output').innerHTML += "<li><img src='https://image.tmdb.org/t/p/w185"+ json.poster_path +"'><p id='close'>X</p></li>";
+            $('body').css('background-image', 'url(http://image.tmdb.org/t/p/original/'+ json.backdrop_path+')');
+            var suppr = document.getElementById('close');
+            var parent = suppr.parentNode;
+            var parentul = parent.parentNode;
+            suppr.addEventListener("click", supression, false);
+            function supression(){
+               parentul.removeChild(parent);
+            }
 
-          // Show the next
-          $imageEls.eq(index).addClass('show');
-
-          // Hide the previous
-          $imageEls.eq(index - 1).removeClass('show');
-      }, 2000);
-  };
-
-  // Document Ready.
-  $(function () {
-      cycleBackgrounds();
-  });
+        },
+        error: function(e) {
+          console.log(e.message);
+        }
+    });
+    }
+}).data("uiAutocomplete")._renderItem = function( ul, tvshow ) {
+var inner_html ="<a>" +  tvshow.name + "</a>";
+return $( "<li></li>" )
+  .data( "item.autocomplete", tvshow )
+  .append(inner_html)
+  .appendTo( ul );
+};
+});
 });
--- www/index.php
+++ www/index.php
@@ -1,3 +1,27 @@
-<?php include("_inc/header.php");?>
-<?php include("_inc/searchbar.php");?>
-<?php include("_inc/footer.php");?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Netflex</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Netflex</h1>
+    <input type="text" id="valeur" placeholder="Ajoutez une série" title="Nom de la série" />
+    <p id="text"></p>
+    <p id="bouton">click</p>
+    <div class="outer">
+              <div class="inner">
+              <header id="TMDBtitle"></header>
+              </div>
+            </div>
+
+      <ul id="output">
+
+      </ul>
+
+    <script src="js/script.js"></script>
+
+</body>
+</html>
